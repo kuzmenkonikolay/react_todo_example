f474aadc92b985634451dd3b2bb56cb3e723d13c
Nikolay Kuzmenko <kuzmenkojob@gmail.com>
2019-04-05 19:37:41 +0300

    add important state

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -9,14 +9,18 @@ export default class TodoListItem extends Component {
     };
 
     onLabelClick = () => {
-        this.setState({
-            done: true
+        this.setState(({done}) => {
+            return {
+                done: !done
+            };
         });
     };
 
     onMarkImportant = () => {
-        this.setState({
-            important: true
+        this.setState(({important}) => {
+            return {
+                important: !important
+            };
         });
     };
 

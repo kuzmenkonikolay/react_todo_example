857c600725f0f88ad6fff04a91e44701d9f135ff
Nikolay Kuzmenko <kuzmenkojob@gmail.com>
2019-04-05 23:30:04 +0300

    add form for creating todo

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -2,21 +2,32 @@ import React, { Component } from 'react';
 import './item-add-form.css'
 
 export default class ItemAddForm extends Component {
-    getInputValue = () => {
-        let title = document.getElementById('new_item_title_input');
-        return title.value;
+
+    state = {
+        label: ''
+    };
+
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        });
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+       this.props.addNewItem(this.state.label);
     };
 
     render() {
         const { addNewItem } = this.props;
 
         return (
-            <div className="item-add-form d-flex">
-                <input type="text" placeholder="Type here new ToDo item" id="new_item_title_input" className="form-control search-input"/>
+            <form className="item-add-form d-flex" onSubmit={this.onSubmit}>
+                <input type="text" placeholder="Type here new ToDo item" onChange={this.onLabelChange} className="form-control search-input"/>
                 <div className="btn-group">
-                    <button type="button" className="btn btn-info" onClick={ () => addNewItem(this.getInputValue()) }>Add Item</button>
+                    <button type="submit" className="btn btn-info">Add Item</button>
                 </div>
-            </div>
+            </form>
         );
     }
 }
